0f3768164878fd4414cbf81cd9d2f2b0a2690a67
I8087 <i8086@protonmail.com>
2017-08-08 00:08:39 -0500

    General update
    Matured the functions.

--- foxxo.c
+++ foxxo.c
@@ -1,11 +1,19 @@
-typedef struct *Foxxo {
-  ;
+#include <stdio.h>
+
+typedef struct Foxxo {
+    int placeholder;
 } Foxxo;
 
-Foxxo Foxxo_Init () {
-  ;
+Foxxo *Foxxo_Init (void) {
+    return (void*) malloc(sizeof(Foxxo));
 }
 
 void Foxxo_Exec(Foxxo foxxo) {
-  ;
+    ;
+}
+
+/* Just a placeholder test. */
+void main(int argc, char *argv[]) {
+    Foxxo *f = Foxxo_Init();
+    return 0;
 }
