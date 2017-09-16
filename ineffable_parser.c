5e1580469ed20acfa684e02a6651b6f85fb363c4
I8087 <i8086@protonmail.com>
2017-09-16 08:59:19 -0500

    Fixed Linux build
    Fixed a problem that prevented compiling on Linux.

--- ineffable_parser.c
+++ ineffable_parser.c
@@ -8,7 +8,7 @@
 void Ineffable_Parser(Ineffable *ineffable, DA *list) {
     DA_print(list);
     for (int i = 0; i < list->len; i++) {
-        Ineffable_BNF b = (Ineffable_BNF) DA_get(list, i);
-        if (b.code) printf("%d: %d:%d - \"%s\"\n", i, b.line, b.chr, b.code);
+        Ineffable_BNF *b = (Ineffable_BNF*) DA_get(list, i);
+        if (b->code) printf("%d: %d:%d - \"%s\"\n", i, b->line, b->chr, b->code);
     }
 }
