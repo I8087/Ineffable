396638101b4a61e9ea33dd1c1d0534ac4b134798
I8087 <i8086@protonmail.com>
2018-08-07 22:19:20 +0900

    Edited build script

--- build-win.cmd
+++ build-win.cmd
@@ -1,3 +1,3 @@
 @echo off
-%tcc%\tcc -o ineffable.exe ineffable.c ineffable_lexer.c ineffable_parser.c DA\da.c -IDA
+%tcc%\tcc -o ineffable.exe src/ineffable.c src/ineffable_lexer.c src/ineffable_lexerRPN.c src/ineffable_parser.c src/ineffable_assembler.c src/ineffable_vm.c DA\da.c -IDA
 pause
